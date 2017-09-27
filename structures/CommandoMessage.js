4c0c313646d6146002f3d7e25d219e02d084aab6
Fina <finapani@gmail.com>
2017-09-27 09:56:27 -0500

    add timothy's unique message reply functionality

--- structures/CommandoMessage.js
+++ structures/CommandoMessage.js
@@ -1,4 +1,16 @@
 const { CommandMessage } = require('discord.js-commando');
+const winston = require('winston');
+
+/**
+ * Responds with a direct message with only an embed.
+ * @param {MessageEmbed} [embed] Embed for the message
+ * @param {?MessageOptions} [options] Options for the message
+ * @return {Promise<Message|Message[]>}
+ */
+CommandMessage.prototype.directEmbed = function(embed, options) {
+	if (!options || typeof options !== 'object') options = {};
+	return this.direct('', Object.assign(options, { embed }));
+};
 
 /**
  * When replying to a message, Timothy is unusual in that he usually deletes the requester's
@@ -6,7 +18,6 @@ const { CommandMessage } = require('discord.js-commando');
  * without removing information.
  * Since the default functionality of CommandMessage does not support such a feature, its reply
  * functions must be overridden.
- * 
  * @see {@link https://github.com/Gawdl3y/discord.js-commando/blob/master/src/commands/message.js}
  */
 
@@ -14,28 +25,86 @@ const superReply = CommandMessage.prototype.reply;
 const superReplyEmbed = CommandMessage.prototype.replyEmbed;
 
 /**
- * @see CommandMessage.reply
+ * Options provided when sending or editing a message.
+ * @typedef {Object} MessageOptionsExt
+ * @extends MessageOptions
+ * @property {string} [argsDisplay=''] Override the command args display. Useful if the command args are too verbose.
+ * Not relevant if the message is a DM.
+ * @param {number} [delTimeout=0] - How long to wait to delete the message in milliseconds
+ */
+
+/**
+ * @param {StringResolvable} [content] Content for the message
+ * @param {?MessageOptionsExt} [options] Options for the message
  * @return {Promise<Message|Message[]>}
+ * @see {@link CommandMessage#reply}
  */
 CommandMessage.prototype.reply = function(content, options) {
-	return superReply.apply(this, [content, options]);
+	this.deleteMsg(options);
+	return superReply.apply(this, [this.buildReply(content, options), options]);
 };
 
 /**
- * @see CommandMessage.replyEmbed
+ * @param {MessageEmbed|Object} [embed] Embed to send
+ * @param {StringResolvable} [content=''] Content for the message
+ * @param {?MessageOptionsExt} [options] Options for the message
  * @return {Promise<Message|Message[]>}
+ * @see {@link CommandMessage#replyEmbed}
  */
 CommandMessage.prototype.replyEmbed = function(embed, content = '', options) {
-	return superReplyEmbed.apply(this, [embed, content, options]);
+	this.deleteMsg(options);
+	return superReplyEmbed.apply(this, [embed, this.buildReply(content, options), options]);
 };
 
 /**
- * Responds with a direct message with only an embed.
- * @param {MessageEmbed} [embed] - Embed for the message
- * @param {MessageOptions} [options] - Options for the message
- * @return {Promise<Message|Message[]>}
+ * Shortcut to `this.delete()` with error catch
+ * @param {?MessageOptionsExt} [options] Options for the message
+ * @returns {Promise<Message>}
+ * @see {@link CommandMessage#delete}
+ * @private
  */
-CommandMessage.prototype.directEmbed = function(embed, options) {
-	if (!options || typeof options !== 'object') options = {};
-	return this.direct('', Object.assign(options, { embed }));
+CommandMessage.prototype.deleteMsg = function(options) {
+	const timeout = options && typeof options === 'object' ? options.timeout : 0;
+	this.delete(timeout)
+		.catch(err => winston.warn(`[DISCORD]: MessageDeleteError > ${err} (Message may have already been deleted)`));
+};
+
+/**
+ * Concat the user's command with Timothy's reply.
+ * @return {string}
+ * @private
+ */
+CommandMessage.prototype.buildReply = function(content, options) {
+	return `${this.getCommandDisplay(options)}${content}`;
+};
+
+/**
+ * Quote the message used to call the command.
+ * @return {string}
+ * @private
+ */
+CommandMessage.prototype.getCommandDisplay = function(options) {
+	if (this.channel.type === 'dm' || this.channel.type === 'group') {
+		return '';
+	}
+
+	if (options && typeof options === 'object' && options.argsDisplay !== '') {
+		return this.buildCustomizedCommandDisplay(options);
+	}
+
+	return `\`${this.cleanContent}\`: `;
+};
+
+/**
+ * Quoting messages should be as accurate as possible. With custom args display, it's necessary to concat the
+ * command name onto the custom args. Using the default command name when the user did not use that specific name
+ * should be avoided to make the quote accurate. Parse the content to find the exact command used before
+ * joining with the custom args display.
+ * @return {string}
+ * @private
+ */
+CommandMessage.prototype.buildCustomizedCommandDisplay = function(options) {
+	const re = new RegExp(`^(.+)${this.argString}`);
+	const cmdUsed = this.cleanContent.match(re)[1];
+	return `\`${cmdUsed} ${options.argsDisplay}\`: `;
 };
