60c8057a1eaba64fbbae4df1ecf94b17e36d449a
Fina <finapani@gmail.com>
2017-09-27 08:45:00 -0500

    merge user-info with old timothy

--- commands/info/user-info.js
+++ commands/info/user-info.js
@@ -1,6 +1,7 @@
 const { Command } = require('discord.js-commando');
 const moment = require('moment');
 const { stripIndents } = require('common-tags');
+const { EMBED_BULLET, EMBED_ARROW } = process.env;
 
 const username = require('../../models/UserName');
 
@@ -10,7 +11,7 @@ module.exports = class UserInfoCommand extends Command {
 			name: 'user-info',
 			aliases: ['user'],
 			group: 'info',
-			memberName: 'user',
+			memberName: 'user-info',
 			description: 'Get info on a user.',
 			details: `Get detailed information on the specified user.`,
 			guildOnly: true,
@@ -34,25 +35,34 @@ module.exports = class UserInfoCommand extends Command {
 		const member = args.member || msg.member;
 		const { user } = member;
 		const usernames = await username.findAll({ where: { userID: user.id } });
-		return msg.embed({
+		const statusMap = {
+			online: `Online :green_heart:`,
+			offline: `Offline :black_heart:`,
+			idle: `Idle :yellow_heart:`,
+			dnd: `Do Not Disturb :heart:`
+		};
+
+		return msg.replyEmbed({
 			color: 3447003,
+			description: `**${user.bot ? 'Bot' : 'User'} ${member} Statistics**`,
+			footer: { text: `User ID: ${user.id}` },
 			fields: [
 				{
-					name: '❯ Member Details',
+					name: `${EMBED_ARROW} Member Details`,
 					value: stripIndents`
-						${member.nickname !== null ? ` • Nickname: ${member.nickname}` : '• No nickname'}
-						• Roles: ${member.roles.map(roles => `\`${roles.name}\``).join(' ')}
-						• Joined at: ${moment.utc(member.joinedAt).format('dddd, MMMM Do YYYY, HH:mm:ss ZZ')}
+						${EMBED_BULLET} **Nickname**: ${member.nickname || 'N/A'}
+						${EMBED_BULLET} **Roles**: ${member.roles.map(roles => `\`${roles.name}\``).join(' ')}
+						${EMBED_BULLET} **Joined**: ${moment.utc(member.joinedAt).format('dddd, MMMM Do YYYY, hh:mm:ss A')} UTC
 					`
 				},
 				{
-					name: '❯ User Details',
+					name: `${EMBED_ARROW} User Details`,
 					/* eslint-disable max-len */
 					value: stripIndents`
-						• Created at: ${moment.utc(user.createdAt).format('dddd, MMMM Do YYYY, HH:mm:ss ZZ')}${user.bot ? '\n• Is a bot account' : ''}
-						• Aliases: ${usernames.length ? usernames.map(uName => uName.username).join(', ') : user.username}
-						• Status: ${user.presence.status}
-						• Game: ${user.presence.game ? user.presence.game.name : 'None'}
+						${EMBED_BULLET} **Created**: ${moment.utc(user.createdAt).format('dddd, MMMM Do YYYY, hh:mm:ss A')} UTC
+						${EMBED_BULLET} **Aliases**: ${usernames.length ? usernames.map(uName => uName.username).join(', ') : user.username}
+						${EMBED_BULLET} **Status**: ${statusMap[user.presence.status] || statusMap.offline}
+						${EMBED_BULLET} **Game**: ${user.presence.game ? user.presence.game.name : 'N/A'}
 					`
 					/* eslint-enable max-len */
 				}
