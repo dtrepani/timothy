d0bde523fdba7f66b6200853b14c5581a683ad5f
Fina <finapani@gmail.com>
2017-10-21 13:43:21 -0500

    add: utility method to create a role and add member to the role

--- util/Util.js
+++ util/Util.js
@@ -1,3 +1,7 @@
+const { Role } = require('discord.js');
+const { FriendlyError } = require('discord.js-commando');
+const { winston } = require('winston');
+
 class Util {
 	constructor() {
 		throw new Error(`The ${this.constructor.name} class may not be instantiated.`);
@@ -35,6 +39,47 @@ class Util {
 
 	static isUrl(item) {
 		return (item.search(/https?:\/\/[^ \/\.]+\.[^ \/\.]+/) !== -1); // eslint-disable-line no-useless-escape
+	}
+
+	/**
+	 * @param {string|Role} role - Role to resolve
+	 * @param {Guild} guild - Guild to resolve against
+	 * @return {Role} - Resolved role
+	 */
+	static resolveRole(role, guild) {
+		if (role instanceof String) {
+			role = guild.roles.find(aRole => aRole.name.toLowerCase() === role.toLowerCase());
+		}
+
+		if (!(role instanceof Role)) {
+			throw new TypeError('ROLE_CONVERT');
+		}
+
+		return role;
+	}
+
+	/**
+	 * Create a role and immediately add a user to the role.
+	 * @param {RoleData} roleData - Data to make role
+	 * @param {GuildMember} member - User to add to role 
+	 * @param {string} [reason] - Reason for making role and adding user to it
+	 * @return {Role}
+	 * @private
+	 */
+	async createAndAddMemberToRole(roleData, member, reason) {
+		try {
+			const role = await member.guild.createRole({
+				data: roleData,
+				reason
+			});
+			await member.addRole(role, reason);
+			return role;
+		} catch (err) {
+			winston.warn(`[DISCORD]: CreateAndAddMemberToRoleError >`, err);
+			throw new FriendlyError(
+				`There was a problem creating the \`${roleData.name}\` role and adding \`${member.displayName}\` to it.`
+			);
+		}
 	}
 }
 
