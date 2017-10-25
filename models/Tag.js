246832b0b32bbfa8923b6e20e65438acfceac197
Fina <finapani@gmail.com>
2017-10-24 21:05:33 -0500

    refine tag model

--- models/Tag.js
+++ models/Tag.js
@@ -2,29 +2,51 @@ const Sequelize = require('sequelize');
 
 const Database = require('../structures/PostgreSQL');
 
-const Tag = Database.db.define('tags', {
-	userID: Sequelize.STRING,
-	userName: Sequelize.STRING,
-	guildID: Sequelize.STRING,
-	guildName: Sequelize.STRING,
-	name: Sequelize.STRING,
-	content: Sequelize.STRING(1800), // eslint-disable-line new-cap
-	type: {
-		type: Sequelize.BOOLEAN,
-		defaultValue: false
+const Tag = Database.db.define(
+	'tags',
+	{
+		userID: Sequelize.STRING,
+		userName: Sequelize.STRING,
+		guildID: Sequelize.STRING,
+		guildName: Sequelize.STRING,
+		channelID: Sequelize.STRING,
+		channelName: Sequelize.STRING,
+		name: {
+			type: Sequelize.STRING,
+			allowNull: true
+		},
+		content: {
+			type: Sequelize.STRING(1800), // eslint-disable-line new-cap
+			allowNull: false
+		},
+		category: {
+			type: Sequelize.STRING,
+			defaultValue: null,
+			allowNull: true
+		},
+		subcategory: {
+			type: Sequelize.STRING,
+			defaultValue: null,
+			allowNull: true
+		},
+		uses: {
+			type: Sequelize.INTEGER,
+			defaultValue: 0
+		}
 	},
-	example: {
-		type: Sequelize.BOOLEAN,
-		defaultValue: false
-	},
-	exampleID: {
-		type: Sequelize.STRING,
-		defaultValue: ''
-	},
-	uses: {
-		type: Sequelize.INTEGER,
-		defaultValue: 0
+	{
+		indexes: [
+			{
+				name: 'IX_Tag_Name_Content_Category_Subcategory',
+				unique: true,
+				fields: ['name', 'content', 'category', 'subcategory']
+			}
+		]
 	}
-});
+);
+
+Tag.prototype.getFullCategory = function() {
+	return `${this.category}${this.subcategory ? `/${this.subcategory}` : ''}`;
+};
 
 module.exports = Tag;
